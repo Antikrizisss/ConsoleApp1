dfffea7797d267f5f6d30d89927ace9b527437ae
Antikrizisss <78258378+Antikrizisss@users.noreply.github.com>
2021-03-05 20:53:00 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine();
+            string text = Console.ReadLine();
+            int x = Convert.ToInt32(text);
+
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("Число 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Число 2");
+                    break;
+
+            }
+
+            Console.WriteLine("Все");
+        }
+    }
+}
