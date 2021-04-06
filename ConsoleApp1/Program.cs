f4d43cea895b52d5a5d7e4f72a5d3a6093cd28ec
Antikrizisss <78258378+Antikrizisss@users.noreply.github.com>
2021-04-07 03:05:40 +0400

    Решил Is this a triangle?

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Triangle.IsTriangle(5, 7, 10));
 
             //Console.WriteLine(Kata.IsValidWalk(new string[] { "n", "n", "s", "s", "n", "s", "n", "s", "n", "s" }));
             
---
+++ ConsoleApp1/Triangle.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ConsoleApp1
+{
+    public class Triangle
+    {
+        //Задача Is this a triangle?
+        public static bool IsTriangle(int a, int b, int c)
+        {
+            if ((a < b + c) & (a > b - c) & (b < a + c) & (b > a - c) & (c < a + b) & (c > a - b))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+    }
+}
